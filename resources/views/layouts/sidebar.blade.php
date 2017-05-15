7f3ff8f4778b0839581338bac0f9e0b9f405c0fd
Nickerchen <jamesfriesen94@gmail.com>
2017-05-15 16:13:18 +0200

    up to lessen 11

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,6 +3,8 @@
     <h4>About</h4>
     <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
   </div>
+
+  
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostsController@index');
 
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
+
+Route::post('/posts', 'PostsController@store');
+//Route::get('/posts/{post}', 'PostsController@show');
