f8797d480360864295ed058997beac49554abb56
Nickerchen <jamesfriesen94@gmail.com>
2017-05-15 14:50:15 +0200

    changed to proper bootstrap example

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostsController@index');
+
+Route::get('/posts/{post}', 'PostsController@show');
