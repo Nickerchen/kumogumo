cc77a7c84b861bc851c224b0d6b642cfba98ef1c
Nickerchen <jamesfriesen94@gmail.com>
2017-06-26 22:29:52 +0200

    changed fonts llocation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,6 @@
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet"  type="text/css">
     </head>
